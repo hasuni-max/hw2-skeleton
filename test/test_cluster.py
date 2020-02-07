2f44dd23b03dc4e7748a43ee4187949c17e95b72
hasuni-max <student@qbc2019-01.ucsfguest.ucsf.edu>
2020-02-07 14:15:32 -0800

    ah

--- test/test_cluster.py
+++ test/test_cluster.py
@@ -2,6 +2,46 @@ from hw2skeleton import cluster
 from hw2skeleton import io, find_features, kmeans, hier, scoring
 import os
 
+def test_similarity():
+    #sign(dist(a,b))==+
+    pdb_ids = [276, 4629]
+
+    active_sites = []
+    for id in pdb_ids:
+        filepath = os.path.join("data", "%i.pdb"%id)
+        active_sites.append(io.read_active_site(filepath))
+    
+    features = find_features.calc_features(active_sites)
+    assert scoring.L2(features['276'],features['4629']) > 0
+
+
+def test_distance_to_self():
+    #dist(a,a)==0
+    pdb_ids = [276]
+
+    active_sites = []
+    for id in pdb_ids:
+        filepath = os.path.join("data", "%i.pdb"%id)
+        active_sites.append(io.read_active_site(filepath))
+    
+    features = find_features.calc_features(active_sites)
+    x = scoring.L2(features['276'],features['276'])
+
+    assert x == 0.0
+
+def test_recip_distance():
+    dist(a,b)==dist(b,a)
+    pdb_ids = [276]
+
+    active_sites = []
+    for id in pdb_ids:
+        filepath = os.path.join("data", "%i.pdb"%id)
+        active_sites.append(io.read_active_site(filepath))
+    
+    features = find_features.calc_features(active_sites)
+
+    assert scoring.L2(features['276'],features['4629']) == scoring.L2(features['4629'],features['276'])
+
 
 
 def test_partition_clustering():
